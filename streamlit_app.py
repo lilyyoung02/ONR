be51e194f565d6a5059689e03eb0926f43e0b610
lilyyoung02 <101677793+lilyyoung02@users.noreply.github.com>
2025-03-16 20:53:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,37 @@ import streamlit as st
 st.set_page_config(page_title="Blood Logistics Tool Input", layout="wide")
 
 st.title("ONR Blood Mangement Support Tool")
-st.sidebar.header("User Input 2")
+st.sidebar.header("User Input")
 
 # Create a dictionary to store user data
 if "user_data" not in st.session_state:
     st.session_state.user_data = []
+with st.form("user_form"):
+    name = st.text_input("Enter your name:")
+    platoonNum = st.number_input("Enter the corresponding platoon number:")
+    currentCombatLevel = st.number_input("Enter the current comabt level:")
+    predictedCompabtLevel = st.number_input("Enter predicted combat level")
 
+    if submit:
+        new_entry = {"name": name, "Platoon Number": platoonNum, "current comabt level": currentCombatLevel, "predicted comabt level": predictedCompabtLevel}
+        st.session_state.user_data.append(new_entry)
+        st.success("Data added successfully!")
+
+# Display stored data
+st.subheader("Stored User Data")
+st.json(st.session_state.user_data)
+
+# Convert data to JSON and provide a download link
+if st.session_state.user_data:
+    json_data = json.dumps(st.session_state.user_data, indent=4)
+    json_file = io.BytesIO(json_data.encode())
+
+    st.download_button(
+        label="Download JSON File",
+        data=json_file,
+        file_name="user_data.json",
+        mime="application/json"
+    )
 #st.title("User Input")
 #st.headers
 #x = st.text_input("Input 1: ")
